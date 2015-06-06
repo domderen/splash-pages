c41f68b155ff8b175fe876258a37d17b4243f96b
Octave <octave.auger@gmail.com>
2015-06-06 14:09:54 +0200

    Added rest of pro dashboard guide in French

--- app/pages/faq/pro-dashboard/mandate-creation/faq-pro-dashboard-mandate-creation.fr.js
+++ app/pages/faq/pro-dashboard/mandate-creation/faq-pro-dashboard-mandate-creation.fr.js
@@ -8,68 +8,32 @@ export default class FaqProDashboardMandateCreationFr extends React.Component {
   render() {
     return (
       <Translation locales='fr'>
-        <h2 className='u-text-heading-light u-text-m u-color-heading'>Mise en place</h2>
+        <h2 className='u-text-heading-light u-text-m u-color-heading'>Création de mandat</h2>
         <p className='para'>
-          GoCardless vous permet d’obtenir des mandats de prélèvement SEPA et de collecter vos 
-          paiement récurrents sans la moindre intégration technique.
+          Vous n'avez pas besoin d'ajouter vos clients vous-mêmes dans le tableau de bord. Ils apparaîtront
+          automatiquement une fois qu'ils auront rempli notre page de mandat. Si vous avez déjà des clients qui
+          vous paient par prélèvement bancaire, contactez-nous pour que nous les importions.
         </p>
 
-        <h3 className='section-heading u-text-heading-light u-color-heading u-margin-Vm u-text-s'>
-          Démarrage
-        </h3>
         <p className='para'>
-          Vous pouvez <a href='https://manage-sandbox.gocardless.com/registrations/create' target='_blank'
-          className='u-link-color-p u-text-underline'>créer un compte test</a> gratuitement. A titre indicatif, 
-          tous les liens dans ce document redirigent vers votre compte test.
-        </p>
-        <p className='para'>
-          Quand vous souhaitez prélever des paiements réels, il nous faut plusieurs documents afin de 
-          pouvoir vous créer un compte actif :
-        </p>
-        <ul className='list'>
-          <li>Signer notre contrat</li>
-          <li>Envoyer un RIB au nom de votre société</li>
-          <li>Envoyer une copie de la carte d’identité ou du passeport de votre gérant et de chaque individu 
-          qui détient plus de 25% du capital de votre société</li>
-        </ul>
-        <p className='para'>
-          Une fois ces documents fournis, nous vous créerons un compte (typiquement sous 24 heures) et vous 
-          pourrez commencer à utiliser GoCardless pour vos clients, sans avoir besoin de passer par votre banque.
-        </p>
-        <p className='para'>
-          Veuillez noter que ce que vous créez dans votre compte test ne sera pas transmis au compte actif.
-        </p>
-        <p className='para'>
-		  Si vous souhaitez migrer des clients avec lesquels vous avez déjà un mandat de prélèvement bancaire, 
-		  veuillez nous contacter.
+          Dans notre <a href='https://manage-sandbox.gocardless.com/customers' target='_blank'
+          className='u-link-color-p u-text-underline'>section Customers</a>, vous possédez un lien que vous pouvez 
+          envoyer à tous vos clients (veuillez nous contacter si vous souhaitez utiliser un mandat papier).
         </p>
 
-        <h3 className='section-heading u-text-heading-light u-color-heading u-margin-Vm u-text-s'>
-          Echéancier SEPA
-        </h3>
+        <img src='/images/fr/pro/create-customer.png' />
+
         <p className='para'>
-          Vous devez créer un paiement quelques jours avant la date à laquelle vous souhaitez prélever votre client :
-        </p>
-        <ul className='list'>
-          <li>6 jours ouvrables si c’est le premier prélèvement pour un nouveau client</li>
-          <li>3 jours ouvrables si c’est un prélèvement ultérieur</li>
-        </ul>
-        <p className='para'>
-          Nous collectons les prélèvements et vous les reversons tous les jours automatiquement : vous recevrez ces 
-          fonds 3 jours ouvrables après la date de paiement du client.
+          Ce lien permet à vos clients d’indiquer leurs données bancaires de manière sécurisée (dans la langue 
+          de leur choix). Cela vous donne l’autorisation de commencer à prélever directement de leur compte en banque.
         </p>
 
-        <h3 className='section-heading u-text-heading-light u-color-heading u-margin-Vm u-text-s'>
-          Préparation du compte
-        </h3>
+        <img src='/images/fr/pro/sepa-mandate-page.png' />
+
         <p className='para'>
-          Vous devez personnaliser votre compte dans <a href='https://manage-sandbox.gocardless.com/organisation/creditors'
-          target='_blank' className='u-link-color-p u-text-underline'>notre section Creditors</a> :
+          La liste de tous vos clients est visible dans la <a href='https://manage-sandbox.gocardless.com/customers' target='_blank'
+          className='u-link-color-p u-text-underline'>même section</a>, et vous pouvez l’exporter vers Excel.
         </p>
-        <ul className='list'>
-          <li>Ajouter un logo qui sera utilisé sur votre page de mandat</li>
-          <li>Ajouter un compte en banque, sur lequel nous verserons vos paiements</li>
-        </ul>
 
       </Translation>
     );
